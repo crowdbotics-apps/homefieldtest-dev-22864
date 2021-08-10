1dc12ae100cb170bde4aff0a8790451f61076a24
Crowdbotics <team@crowdbotics.com>
2021-08-10 20:13:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "homefieldtest_dev_22864Identifier",
+  url: "https://homefieldtest_dev_22864Identifier.botics.co",
+  api: "https://homefieldtest_dev_22864Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
